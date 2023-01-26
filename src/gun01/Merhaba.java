ea615ae1c974344c9323f775fd120eee498ed2ab
User <enesaydin42@gmail.com>
2023-01-26 18:22:23 +0100

    gun 01 sysout gosterildi 2

---
+++ src/gun01/Merhaba.java
@@ -0,0 +1,10 @@
+package gun01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("mrehaba");
+        System.out.println("merhaba 2");
+
+    }
+}
